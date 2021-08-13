3ec468192535be71e092f4b0f2d3f23c6f925e09
michael carini <snownkc@icloud.com>
2021-08-13 14:48:17 +0100

    added new repo

---
+++ SerialisationApp/SerialisationApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SerialisationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
